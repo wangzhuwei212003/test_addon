05bd3515874ebfdc5e46d5bcd97eb281b099f3ef
zhuwei wang <wangzhuwei212003@163.com>
2019-10-21 16:18:08 +0800

    export functions with input parameters

--- cppsrc/Samples/functionexample.cpp
+++ cppsrc/Samples/functionexample.cpp
@@ -4,7 +4,6 @@ std::string functionexample::hello()
 {
   return "He";
 }
-
 Napi::String functionexample::HelloWrapped(const Napi::CallbackInfo &info)
 {
   Napi::Env env = info.Env();
@@ -12,14 +11,32 @@ Napi::String functionexample::HelloWrapped(const Napi::CallbackInfo &info)
   return returnValue;
 }
 
+int functionexample::add(int a, int b)
+{
+  return a + b;
+}
+Napi::Number functionexample::AddWrapped(const Napi::CallbackInfo &info)
+{
+  Napi::Env env = info.Env();
+  if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber())
+  {
+    Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
+  }
+
+  Napi::Number first = info[0].As<Napi::Number>();
+  Napi::Number second = info[1].As<Napi::Number>();
+
+  int returnValue = functionexample::add(first.Int32Value(), second.Int32Value());
+  return Napi::Number::New(env, returnValue);
+}
+
 Napi::Object functionexample::Init(Napi::Env env, Napi::Object exports)
 {
-  exports.Set(
-    "hello", Napi::Function::New(env, functionexample::HelloWrapped)
-  );
+  exports.Set("hello", Napi::Function::New(env, functionexample::HelloWrapped));
+  exports.Set("add", Napi::Function::New(env, functionexample::AddWrapped));
   return exports;
 }
 
-// For every function in C++ we want to export 
-// we will basically create a NAPI wrapped function (HelloWrapped in this example) 
+// For every function in C++ we want to export
+// we will basically create a NAPI wrapped function (HelloWrapped in this example)
 // and add it to the exports object using Init.
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const testAddon = require('./build/Release/testaddon.node');
 
 console.log('addon', testAddon);
 console.log('addon', testAddon.hello());
+console.log('add', testAddon.add(5, 10));
 
 module.exports = testAddon;
