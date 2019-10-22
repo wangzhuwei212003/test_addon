8ede966963c10ca4cd8909446f7a9c56ad817886
zhuwei wang <wangzhuwei212003@163.com>
2019-10-22 15:46:22 +0800

    Sending complex js objects to the C++ world

--- cppsrc/Samples/classexample.cpp
+++ cppsrc/Samples/classexample.cpp
@@ -24,36 +24,54 @@ ClassExample::ClassExample(const Napi::CallbackInfo &info) : Napi::ObjectWrap<Cl
   Napi::HandleScope scope(env);
 
   int length = info.Length();
-  if (length != 1 || !info[0].IsNumber())
+  // if (length != 1 || !info[0].IsNumber())
+  // {
+  //   Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
+  // }
+  if (length != 1)
   {
-    Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
+    Napi::TypeError::New(env, "Only one argument expected").ThrowAsJavaScriptException();
+  }
+  if (!info[0].IsNumber())
+  {
+    Napi::Object object_parent = info[0].As<Napi::Object>();
+    ClassExample* example_parent = Napi::ObjectWrap<ClassExample>::Unwrap(object_parent);
+    ActualClass* parent_actual_class_instance = example_parent->GetInternalInstance();
+    _actualClass = new ActualClass(parent_actual_class_instance->getValue()); // 其实这里 new 一个 class 没有改变 actual class 的构造。
+    return;
   }
 
   Napi::Number value = info[0].As<Napi::Number>();
-  _actualClass = new ActualClass(value.DoubleValue());
+  _actualClass = new ActualClass(value.DoubleValue()); // 这里是能够接收两种类型的参数。
 }
 
-Napi::Value ClassExample::GetValue(const Napi::CallbackInfo& info) {
+Napi::Value ClassExample::GetValue(const Napi::CallbackInfo &info)
+{
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
 
-  // double num = _actualClass.getValue(); 
+  // double num = _actualClass.getValue();
   // member reference type 'ActualClass *' is a pointer; did you mean to use '->'?
   double num = _actualClass->getValue();
   return Napi::Number::New(env, num);
 }
 
-Napi::Value ClassExample::Add(const Napi::CallbackInfo& info) {
+Napi::Value ClassExample::Add(const Napi::CallbackInfo &info)
+{
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
 
-  if (  info.Length() != 1 || !info[0].IsNumber()) {
+  if (info.Length() != 1 || !info[0].IsNumber())
+  {
     Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
   }
 
-  Napi::Number toAdd = info[0].As<Napi::Number>(); 
+  Napi::Number toAdd = info[0].As<Napi::Number>();
   double answer = _actualClass->add(toAdd.DoubleValue());
 
   return Napi::Number::New(info.Env(), answer);
 }
 
+ActualClass* ClassExample::GetInternalInstance(){
+  return _actualClass;
+}
--- cppsrc/Samples/classexample.h
+++ cppsrc/Samples/classexample.h
@@ -6,7 +6,8 @@ class ClassExample : public Napi::ObjectWrap<ClassExample>
 public:
   static Napi::Object Init(Napi::Env env, Napi::Object exports); // Init function for setting the export key to JS
   ClassExample(const Napi::CallbackInfo &info);                  // constructor to initialise
-
+  ActualClass* GetInternalInstance();
+  
 private:
   static Napi::FunctionReference constructor; // reference to store the class definition that needs to be exported to JS
   Napi::Value GetValue(const Napi::CallbackInfo &info);
